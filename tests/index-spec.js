67686039c2c5d78848c4ba25d7870375a6d41fc2
Treudey <alextreude@hotmail.com>
2019-05-14 21:01:18 -0400

    created test file

---
+++ tests/index-spec.js
@@ -0,0 +1,16 @@
+import { describe } from 'mocha';
+
+const chai = require('chai');
+
+const { expect } = chai;
+const server = require('../server');
+
+// eslint-disable-next-line no-console
+console.log('test file, server = \n', server);
+
+describe('test', () => {
+  it('this simple test should return a string', () => {
+    expect('Test CI with Travis').to.equal('Test CI with Travis');
+  });
+  process.exit();
+});
