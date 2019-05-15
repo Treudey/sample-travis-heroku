d81946f75de02d94bfb1bbb1263dd150f349956d
Treudey <alextreude@hotmail.com>
2019-05-14 21:57:06 -0400

    updated server.js message

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.get('/', (req, res) => {
   // eslint-disable-next-line no-console
   console.log('req = \n', req);
-  res.send('Test CI with Travis');
+  res.send('Test CI with Alex and Travis');
 });
 
 const server = app.listen(PORT, () => {
